1ae4aa1615c2bb0d2b0ffc7734e642c20325daf3
Rickard <zupher@gmail.com>
2012-02-13 15:00:00 +0100

    poll listing update and add poll working

--- js/poll.js
+++ js/poll.js
@@ -1,15 +1,10 @@
 var Poll = Backbone.Model.extend({
-<<<<<<< HEAD
     defaults : {
-=======
-    defaults: {
->>>>>>> origin/master
         "id": null,
         "question": "",
         "answers": {},
         "status": false,
         "private": false,
-<<<<<<< HEAD
         "date": 1970-01-01
     },
     
@@ -22,121 +17,68 @@ var Poll = Backbone.Model.extend({
 
 var PollView = Backbone.View.extend({     
     render : function() {
-        this.el.innerHTML = "<a href='#poll/" + this.model.get('id') + "'>" + this.model.get('id') + "</a>";
+        console.log("PollView.render");
+        this.el.innerHTML = "<a href='#poll/" + this.model.get('id') + "'>" + this.model.get('question') + "</a>";
         return this;
     }
 });
 
 var PollCollectionView = Backbone.View.extend({
     initialize : function() {
-        var that = this;
+        console.log("PollCollectionView.initialize");
+        _(this).bindAll('add');
         this._pollViews = [];
         
-        this.collection.each(function( poll ) {
-            that._pollViews.push(new UpdatingPollView({
-                model : poll,
-                tagName : 'li'
-            }));
+        this.collection.each(this.add);
+        
+        this.collection.bind('add', this.add);
+    },
+    
+    events: {
+        "click .create-poll": "createPoll"
+    },
+    
+    createPoll: function( event ) {
+        console.log("PollCollectionView.newPoll");
+        new CreatePollView({
+            collection: this.collection
         });
     },
-     
+    
+    add : function( poll ) {
+        console.log("PollCollectionView.add");
+        var updatingPollView = new UpdatingPollView({
+            tagName : 'li',
+            model : poll
+        });
+        
+        this._pollViews.push(updatingPollView);
+        
+        $(this.el).append(updatingPollView.render().el);
+    },
+    
     render : function() {
+        console.log("PollCollectionView.render");
         var that = this;
         $(this.el).empty();
+        
+        var template = _.template( $("#main-template").html() );
+        $(this.el).html( template );
         
         _(this._pollViews).each(function( pollView ) {
             $(that.el).append(pollView.render().el);
         });
+        
+        return this;
     }
 });
 
 var UpdatingPollView = PollView.extend({
     initialize : function( options ) {
-        this.render = _.bind(this.render, this); 
- 
+        console.log("UpdatingPollView.initialize");
+        this.render = _.bind(this.render, this);
+        
         this.model.bind('change:question', this.render);
-    }
-});
-
-var PollRouter = Backbone.Router.extend({
-    routes : {
-        "poll/:id" : "poll"
-    },
- 
-    poll : function( id ) {
-        console.log(polls.get(id).get('question'));
-    }
-});
-
-var Polls = Backbone.Collection.extend({
-    model: Poll,
-    url: "/polls"
-});
-
-var polls = new Polls([
-    { "id": 1, "question": "Wich came first the chicken or the egg?", "answers": {}, "status": true, "private": false, "date": 2012-02-10 },
-    { "id": 2, "question": "Was the shovel a groundbreaking invention?", "answers": {}, "status": true, "private": false, "date": 2012-02-10 }
-]);
-
-var pollCollectionView = new PollCollectionView({
-    collection: polls,
-    el: $('ul.polls')[0]
-});
-
-pollCollectionView.render();
-
-var updatingPollView = new UpdatingPollView({
-    model: polls
-});
-
-var renderedPoll = updatingPollView.render().el;
-
-var pollRouter = new PollRouter({
-    collection: polls
-});
-Backbone.history.start();
-=======
-        "date": 1970-01-01,
-        "code": ""
-    },
-    
-    validate: function(attribs){
-        if(attribs.question === ""){
-            this.isValid = false;
-            return "You must enter a question.";
-        }
-    },
-    
-    initialize: function(){
-        console.log("Poll model initialized");
-        this.bind("error", function(model, error){
-            console.log(error);
-        });
-    }
-});
-
-var MainView = Backbone.View.extend({
-    el: $('#content'),
-    
-    initialize: function() {
-        console.log("MainView initialized");
-        this.render();
-    },
-    
-    render: function( event ) {
-        $(this.el).empty();
-        var template = _.template( $("#polls_template").html() );
-        console.log(pollCollection.toJSON());
-        $(this.el).html( template(pollCollection.toJSON()) );
-        return this;
-    },
-    
-    events: {
-        "click .new-poll-btn": "createPoll"
-    },
-    
-    createPoll: function ( event ) {
-        new CreatePollView();
     }
 });
 
@@ -150,7 +92,7 @@ var CreatePollView = Backbone.View.extend({
  
     render: function( event ) {
         $(this.el).empty();
-        var template = _.template( $("#new_poll_template").html() );
+        var template = _.template( $("#new-poll-template").html() );
         $(this.el).html( template );
         return this;
     },
@@ -161,7 +103,7 @@ var CreatePollView = Backbone.View.extend({
     },
     
     addAnswer: function ( event ) {
-        console.log("NotYetImplemented")
+        console.log("NotYetImplemented");
     },
     
     newPoll: function ( event ) {
@@ -172,19 +114,51 @@ var CreatePollView = Backbone.View.extend({
         });
         poll.set( { id: $("#temp-id").val() , question: $("#question").val() } );
         
-        if (poll.isValid) {
-            $(this.el).undelegate('.create-poll-btn', 'click');
-            pollCollection.add(poll);
-            console.log("Poll added to collection");
-            mainView.render();
-        }
+        $(this.el).undelegate('.create-poll-btn', 'click');
+        this.collection.add(poll);
+        console.log("Poll added to collection");
+        pollCollectionView.render();
     }
 });
 
-var PollCollection = Backbone.Collection.extend({
-    model: Poll
+var PollRouter = Backbone.Router.extend({
+    routes : {
+        "poll/:id" : "poll"
+    },
+ 
+    poll : function( id ) {
+        console.log("PollRouter.poll")
+        console.log(polls.get(id));
+    }
 });
 
-var pollCollection = new PollCollection();
-var mainView = new MainView();
->>>>>>> origin/master
+var Polls = Backbone.Collection.extend({
+    model: Poll,
+    url: "/polls"
+});
+
+/** END OF FUNCTIONS **/
+
+var polls = new Polls([
+    { "id": 1, "question": "Wich came first the chicken or the egg?", "answers": {}, "status": true, "private": false, "date": 2012-01-01 },
+    { "id": 2, "question": "Was the shovel a groundbreaking invention?", "answers": {}, "status": true, "private": false, "date": 2012-01-01 }
+]);
+
+var pollCollectionView = new PollCollectionView({
+    collection: polls,
+    el: $('#content')
+});
+
+pollCollectionView.render();
+
+var pollRouter = new PollRouter({
+    collection: polls
+});
+
+Backbone.history.start();
+
+polls.add([
+    { "id": 3, "question": "Is this the run?", "answers": {}, "status": true, "private": false, "date": 2012-01-01 }
+]);
+
+polls.get(1).set( { "question": "Le change workzie?" } );
